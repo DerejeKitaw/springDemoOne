c1b78113371ea9464648fc9246c8c1b3d5167918
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-10-24 20:15:26 -0400

    data added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
 use springbootdemo;
 insert into `users`(`user_id`,`user_name`,`password`) values
-('user id 1','user name 1','password1');
+('user id 1','user name 1','password1'),
+('user id 2','user name 2','password2');
